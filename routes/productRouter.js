2513e826c85f8ea7652ad18f83b04ad0a93c6b7c
nileshkumar12 <nilesh.kumar12@gmail.com>
2026-08-02 15:23:43 +0530

    Agent Host changes for agents/add-product-image-multer

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const upload = require('../utils/upload');
 const {
   getProducts,
   getProductById,
@@ -11,8 +12,8 @@ const {
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', auth, createProduct);
-router.put('/:id', auth, updateProduct);
+router.post('/', auth, upload.single('image'), createProduct);
+router.put('/:id', auth, upload.single('image'), updateProduct);
 router.delete('/:id', auth, deleteProduct);
 
 module.exports = router;
