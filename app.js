59da564160e4008c0a5a1cc600a8f0fdb8ec2416
nileshkumar12 <nilesh.kumar12@gmail.com>
2026-08-13 14:18:53 +0530

    cors enabled

--- app.js
+++ app.js
@@ -19,10 +19,23 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 
 
 app.use(express.json());
